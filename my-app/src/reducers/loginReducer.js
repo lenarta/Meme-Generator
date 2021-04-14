8a358987aeb8b01b7c826edb488f91fcf7f556a3
András Lénárt <lenart.andraselod@gmail.com>
2021-04-14 07:46:48 +0200

    redux

---
+++ my-app/src/reducers/loginReducer.js
@@ -0,0 +1,35 @@
+import {
+  LOAD_USER_TOKEN,
+  LOAD_USER_TOKEN_PAYLOAD,
+  DELETE_STORE,
+} from '../constants/actionTypes';
+
+const initialState = {
+  token: '',
+  data: {
+    id: null,
+    userName: '',
+    iat: null,
+  },
+};
+
+function loginReducer(state = initialState, action) {
+  if (action.type === LOAD_USER_TOKEN) {
+    return {
+      ...state,
+      token: action.payload,
+    };
+  }
+  if (action.type === LOAD_USER_TOKEN_PAYLOAD) {
+    return {
+      ...state,
+      data: action.payload,
+    };
+  }
+  if (action.type === DELETE_STORE) {
+    return initialState;
+  }
+  return state;
+}
+
+export default loginReducer;
