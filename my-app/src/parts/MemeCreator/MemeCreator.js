9ad6caef23dbda7d4ea6538e3958d1c92af9d72f
BitiOne <bittner.school@gmail.com>
2021-04-14 17:51:02 +0200

    Meme generator working, upload and download NOT!

--- my-app/src/parts/MemeCreator/MemeCreator.js
+++ my-app/src/parts/MemeCreator/MemeCreator.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import ReactDOM from 'react-dom';
 import { useSelector } from 'react-redux';
 import './MemeCreator.css';
 
 const MemeCreator = () => {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  
   const imageUrl = useSelector(state => state.picture.pictureUrl);
 
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    console.log('MUHAHAHAHAAA');
+    localStorage.setItem("firstName", "Mark Zuker berg");
+  }
+
   return (
-    <div>
-      <h1>
-      {imageUrl}
-      </h1>
-      This is the MemeCreator
+    <div className='meme-creator-container'>
+      <div className='meme-creator-header'>
+        <img className='meme-creator-header-img'
+          src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
+          alt='Problem?'
+        />
+        <p className='meme-creator-header-text'>Meme Generator</p>
+      </div>
+      <div className='meme-creator-body'>
+      <form className="meme-creator-form">
+        <input
+          type="text"
+          name="topText"
+          placeholder="Top Text"
+          value={topText}
+          onChange={e => setTopText(e.target.value)}
+        />
+        <input
+          type="text"
+          name="bottomText"
+          placeholder="Bottom Text"
+          value={bottomText}
+          onChange={e => setBottomText(e.target.value)}
+        />
+        <button onClick={handleClick}>Download</button>
+      </form>
+      <div className='meme-creator-preview-container'>
+        <p className='meme-creator-preview-text-top'>{topText}</p>
+        <img src={imageUrl} alt='X' className='meme-creator-preview-img'/>
+        <p className='meme-creator-preview-text-bottom'>{bottomText}</p>
+      </div>
+      </div>
     </div>
   );
 };
